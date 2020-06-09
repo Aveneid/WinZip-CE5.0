42f9d6ff2011289285a9140bae6197aca0c14b32
Aveneid <dj.exers@gmail.com>
2020-06-09 22:10:27 +0200

    initial

---
+++ SmartDeviceProject2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SmartDeviceProject2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItem1 = new System.Windows.Forms.MenuItem();
+            this.menuItem2 = new System.Windows.Forms.MenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItem1);
+            // 
+            // menuItem1
+            // 
+            this.menuItem1.MenuItems.Add(this.menuItem2);
+            this.menuItem1.Text = "File";
+            // 
+            // menuItem2
+            // 
+            this.menuItem2.Text = "Path";
+            this.menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(240, 268);
+            this.listView1.TabIndex = 0;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            // 
+            // statusBar1
+            // 
+            this.statusBar1.Location = new System.Drawing.Point(0, 246);
+            this.statusBar1.Name = "statusBar1";
+            this.statusBar1.Size = new System.Drawing.Size(240, 22);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.statusBar1);
+            this.Controls.Add(this.listView1);
+            this.Menu = this.mainMenu1;
+            this.Name = "Form1";
+            this.Text = "C\'kurwozaury";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MainMenu mainMenu1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuItem menuItem1;
+        private System.Windows.Forms.MenuItem menuItem2;
+        private System.Windows.Forms.StatusBar statusBar1;
+
+
+    }
+}
+
---
+++ SmartDeviceProject2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace SmartDeviceProject2
+{
+    public partial class Form1 : Form
+    {
+        private String currentPath = "\\";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateListView("\\");
+        }
+        public void populateListView(String path){
+            statusBar1.Text = currentPath;
+            listView1.Clear();
+            listView1.Items.Add(new ListViewItem(".."));
+            try
+            {
+                String[] dirs = Directory.GetDirectories(path);
+                String[] files = Directory.GetFiles(path);
+
+                foreach (String d in dirs)
+                {
+                    String s = d.Substring(d.LastIndexOf('\\'));
+                    ListViewItem i = new ListViewItem(s);
+                    listView1.Items.Add(i);
+                }
+                foreach (String f in files)
+                {
+                    ListViewItem i = new ListViewItem(Path.GetFileName(f));
+                    listView1.Items.Add(i);
+                }
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("IOEXCEPTION" + ex.StackTrace);
+            }
+
+        }
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            /*String path = Microsoft.VisualBasic.Interaction.InputBox("path", "path", "/", 0, 0);
+            populateListView(path);
+             * */
+           // populateListView("/SDMMC");
+        }
+
+        private String getSelectedItem()
+        {
+            Debug.Write("\r\n"+listView1.Items[listView1.SelectedIndices[0]].Text);
+            return listView1.Items[listView1.SelectedIndices[0]].Text;
+        }
+
+        private void listView1_ItemActivate(object sender, EventArgs e)
+        {
+            String item = getSelectedItem();
+            if (item== "..")
+            {
+                Debug.Write(currentPath);
+                currentPath = currentPath.Substring(currentPath.IndexOf('\\'), currentPath.LastIndexOf('\\'));
+                currentPath = '\\' + currentPath;
+                //currentPath = currentPath.Substring(1);
+                populateListView(currentPath);
+            }
+            if (item.Substring(0, 1) == "\\")
+            {
+                currentPath += item;
+                currentPath = currentPath.Substring(1);
+                Debug.Write(" " + currentPath + " "); 
+                populateListView(currentPath);
+            }
+            
+        }
+
+    }
+}
