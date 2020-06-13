c7da768686ec4015a49ba4f515166d3ae2e37591
Aveneid <dj.exers@gmail.com>
2020-06-13 15:30:01 +0200

    .

--- SmartDeviceProject2/Form1.Designer.cs
+++ SmartDeviceProject2/Form1.Designer.cs
@@ -28,11 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.menuItem1 = new System.Windows.Forms.MenuItem();
             this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
             this.statusBar1 = new System.Windows.Forms.StatusBar();
+            this.toolBar1 = new System.Windows.Forms.ToolBar();
+            this.imageList1 = new System.Windows.Forms.ImageList();
+            this.parent = new System.Windows.Forms.ToolBarButton();
+            this.addfolder = new System.Windows.Forms.ToolBarButton();
+            this.delete = new System.Windows.Forms.ToolBarButton();
+            this.refresh = new System.Windows.Forms.ToolBarButton();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -51,31 +61,91 @@
             // 
             // listView1
             // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.Add(this.columnHeader1);
+            this.listView1.Columns.Add(this.columnHeader3);
+            this.listView1.Columns.Add(this.columnHeader4);
             this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView1.Location = new System.Drawing.Point(0, 0);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(240, 268);
+            this.listView1.Size = new System.Drawing.Size(240, 294);
             this.listView1.TabIndex = 0;
-            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.View = System.Windows.Forms.View.Details;
             this.listView1.ItemActivate += new System.EventHandler(this.listView1_ItemActivate);
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 154;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Date";
+            this.columnHeader3.Width = 60;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Size";
+            this.columnHeader4.Width = 60;
             // 
             // statusBar1
             // 
-            this.statusBar1.Location = new System.Drawing.Point(0, 246);
+            this.statusBar1.Location = new System.Drawing.Point(0, 272);
             this.statusBar1.Name = "statusBar1";
             this.statusBar1.Size = new System.Drawing.Size(240, 22);
+            // 
+            // toolBar1
+            // 
+            this.toolBar1.Buttons.Add(this.parent);
+            this.toolBar1.Buttons.Add(this.addfolder);
+            this.toolBar1.Buttons.Add(this.refresh);
+            this.toolBar1.Buttons.Add(this.delete);
+            this.toolBar1.ImageList = this.imageList1;
+            this.toolBar1.Name = "toolBar1";
+            this.toolBar1.ButtonClick += new System.Windows.Forms.ToolBarButtonClickEventHandler(this.toolBar1_ButtonClick);
+            this.imageList1.Images.Clear();
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource"))));
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource1"))));
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource2"))));
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource3"))));
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource4"))));
+            this.imageList1.Images.Add(((System.Drawing.Image)(resources.GetObject("resource5"))));
+            // 
+            // parent
+            // 
+            this.parent.ImageIndex = 1;
+            this.parent.ToolTipText = "Parent folder";
+            // 
+            // addfolder
+            // 
+            this.addfolder.ImageIndex = 0;
+            this.addfolder.ToolTipText = "Create folder";
+            // 
+            // delete
+            // 
+            this.delete.ImageIndex = 3;
+            this.delete.ToolTipText = "Delete selected";
+            // 
+            // refresh
+            // 
+            this.refresh.ImageIndex = 2;
+            this.refresh.ToolTipText = "Refresh";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.ClientSize = new System.Drawing.Size(240, 294);
+            this.Controls.Add(this.toolBar1);
             this.Controls.Add(this.statusBar1);
             this.Controls.Add(this.listView1);
+            this.Location = new System.Drawing.Point(0, 0);
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
-            this.Text = "C\'kurwozaury";
+            this.Text = "WinZip";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
@@ -87,6 +157,15 @@
         private System.Windows.Forms.MenuItem menuItem1;
         private System.Windows.Forms.MenuItem menuItem2;
         private System.Windows.Forms.StatusBar statusBar1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ToolBar toolBar1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ToolBarButton parent;
+        private System.Windows.Forms.ToolBarButton addfolder;
+        private System.Windows.Forms.ToolBarButton refresh;
+        private System.Windows.Forms.ToolBarButton delete;
 
 
     }
--- SmartDeviceProject2/Form1.cs
+++ SmartDeviceProject2/Form1.cs
@@ -14,38 +14,57 @@ namespace SmartDeviceProject2
 {
     public partial class Form1 : Form
     {
+        public String bck = @"\";
         private String currentPath = "\\";
 
 
         public Form1()
         {
             InitializeComponent();
+            
             populateListView("\\");
         }
         public void populateListView(String path){
             statusBar1.Text = currentPath;
-            listView1.Clear();
-            listView1.Items.Add(new ListViewItem(".."));
+            listView1.Items.Clear();
+           // listView1.Items.Add(new ListViewItem(".."));
             try
             {
                 String[] dirs = Directory.GetDirectories(path);
-                String[] files = Directory.GetFiles(path);
-
+                FileInfo[] files = new DirectoryInfo(path).GetFiles();
+                
                 foreach (String d in dirs)
                 {
-                    String s = d.Substring(d.LastIndexOf('\\'));
-                    ListViewItem i = new ListViewItem(s);
+                    String name = d.Substring(d.LastIndexOf('\\'));
+                    ListViewItem i = new ListViewItem(name);
+                    i.SubItems.Add("");
+                    i.SubItems.Add("<DIR>");
                     listView1.Items.Add(i);
                 }
-                foreach (String f in files)
+                 
+                foreach (FileInfo f in files)
                 {
-                    ListViewItem i = new ListViewItem(Path.GetFileName(f));
+                    ListViewItem i = new ListViewItem();
+                    i.Text = f.Name;
+                    if(currentPath.Substring(currentPath.Length-1)=="\\")
+                        i.SubItems.Add(File.GetLastWriteTime(currentPath + f.Name).ToString());
+                    else
+                        i.SubItems.Add(File.GetLastWriteTime(currentPath +"\\"+ f.Name).ToString());
+                        //String g = currentPath + @"\" + f.Name;
+                    
+                    
+                    i.SubItems.Add(f.Length.ToString() + " bytes");
+                    
                     listView1.Items.Add(i);
                 }
             }
             catch (IOException ex)
             {
-                MessageBox.Show("IOEXCEPTION" + ex.StackTrace);
+                //Debug.Write(f.Name);
+                Debug.Write("\r\n=============================");
+                Debug.Write("\nIOEXCEPTION " + ex.GetBaseException() + "\n");
+                Debug.Write(ex.StackTrace + "\n");
+                Debug.Write("=============================");
             }
 
         }
@@ -59,30 +78,51 @@ namespace SmartDeviceProject2
 
         private String getSelectedItem()
         {
-            Debug.Write("\r\n"+listView1.Items[listView1.SelectedIndices[0]].Text);
+           Debug.Write("\r\n"+listView1.Items[listView1.SelectedIndices[0]].Text);
             return listView1.Items[listView1.SelectedIndices[0]].Text;
         }
 
         private void listView1_ItemActivate(object sender, EventArgs e)
         {
             String item = getSelectedItem();
-            if (item== "..")
+           /* if (item== "..")
             {
-                Debug.Write(currentPath);
+               // Debug.Write("\r\n"+";"+currentPath+";"+"\r\n");
                 currentPath = currentPath.Substring(currentPath.IndexOf('\\'), currentPath.LastIndexOf('\\'));
-                currentPath = '\\' + currentPath;
+                if(currentPath == "")
+                    currentPath = '\\' + currentPath;
+
                 //currentPath = currentPath.Substring(1);
                 populateListView(currentPath);
-            }
+                return;
+            }*/
             if (item.Substring(0, 1) == "\\")
             {
-                currentPath += item;
-                currentPath = currentPath.Substring(1);
-                Debug.Write(" " + currentPath + " "); 
+                item = item.Substring(1);
+                //Debug.Write("\r\n" + ";" + currentPath + ";" + "\r\n");
+                if(currentPath != "\\")
+                    currentPath += '\\' + item;
+                else
+                    currentPath += item;
+
+                //Debug.Write(" " + currentPath + " "); 
                 populateListView(currentPath);
+                return;
             }
             
         }
 
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+           
+           //listView1.Items = new Sort
+        }
+
+        private void toolBar1_ButtonClick(object sender, ToolBarButtonClickEventArgs e)
+        {
+            e.Button.
+
+
+        }
     }
 }
